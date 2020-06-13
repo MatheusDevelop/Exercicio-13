628f285f2632a23582a89083e257f16be4a42cf5
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-06-13 12:32:31 -0300

    ...

---
+++ Calcular.cs
@@ -0,0 +1,16 @@
+using System;
+namespace ex2
+{
+    public class Calcular
+    {
+        public void Soma(int num1){
+            Console.WriteLine(num1+num1);
+        }
+        public void Soma(int num1,int num2){
+            Console.WriteLine(num1+num2);
+        }
+        public void Soma(string num1,string num2){
+            Console.WriteLine(num1+num2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Calcular conta = new Calcular();
+            int num1,num2;
+            Console.WriteLine("Digite um numero para saber seu dobro");
+            num1=int.Parse(Console.ReadLine());
+            conta.Soma(num1);
+            Console.WriteLine("Digite dois numeros para saber sua soma");
+            num1=int.Parse(Console.ReadLine());
+            num2=int.Parse(Console.ReadLine());
+            conta.Soma(num1,num2);
+            Console.WriteLine("Digite dois numeros para concatenar");
+            string n1=Console.ReadLine();
+            string n2=Console.ReadLine();
+            conta.Soma(n1,n2);
+
+
+
+
+        }
+    }
+}
